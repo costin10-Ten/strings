e0044d61e8ab4eb0d6e8aee86edc16d1641132a9
costin10-Ten <10costin@gmail.com>
2026-03-14 08:52:09 +0800

    Fix GitHub URL casing and base path format

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   // 1. 這裡一定要填你的 GitHub 網址，結尾不要有斜線
-  site: 'https://costin10-Ten.github.io',
+  site: 'https://costin10-ten.github.io',
   
   // 2. 這裡填你的 Repository 名字，前後都要有斜線
-  base: '/strings/', 
+  base: '/strings', 
   
   integrations: [sitemap()],
 });
